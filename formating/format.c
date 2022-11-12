9f39d8a143abaca3cf8406851941049a7d1e0b26
Riescent <91223304+Riescent@users.noreply.github.com>
2022-11-12 22:49:22 +0100

    Seperated main loop in another function

--- formating/format.c
+++ formating/format.c
@@ -6,16 +6,18 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 21:48:56 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/12 22:22:59 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/12 22:46:39 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
+#include <stdarg.h>
 #include <stdlib.h>
 
-char	*format(const char *str_format, int *char_written)
+char	*format(const char *str_format, int *char_written, va_list *args)
 {
 	(void)char_written;
+	(void)args;
 	if (*str_format == '\0')
 		return (NULL);
 	if (*str_format == '%')
--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,19 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 21:32:51 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/12 21:49:12 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/12 22:46:20 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 100
 
 char	*get_final_str(t_list *line_lst);
-char	*format(const char *str_format, int *char_written);
+char	*format(const char *str_format, int *char_written, va_list *args);
 
 char	*strdup_till_format(const char *str_format, int *char_written)
 {
@@ -30,34 +31,42 @@ char	*strdup_till_format(const char *str_format, int *char_written)
 	return (ft_substr(str_format, 0, result_end - str_format));
 }
 
-int	error(t_list *str_list)
+t_list	*get_str_list(const char *str_format, va_list *args, int *char_written)
 {
-	ft_lstclear(&str_list, &free);
-	return (-1);
+	int		previous_char_written;
+	char	*str;
+	t_list	*str_list;
+
+	str_list = NULL;
+	*char_written = 0;
+	while (*str_format)
+	{
+		previous_char_written = *char_written;
+		if (*str_format == '%')
+			str = format(str_format + 1, char_written, args);
+		else
+			str = strdup_till_format(str_format, char_written);
+		if (str == NULL)
+		{
+			ft_lstclear(&str_list, free);
+			return (NULL);
+		}
+		else
+			ft_lstadd_front(&str_list, ft_lstnew(str));
+		str_format += *char_written - previous_char_written;
+	}
+	return (str_list);
 }
 
 int	ft_printf(const char *str_format, ...)
 {
 	int		char_written;
-	int		previous_char_written;
 	t_list	*str_list;
-	char	*str;
+	va_list	args;
 
-	str_list = NULL;
-	char_written = 0;
-	while (*str_format)
-	{
-		previous_char_written = char_written;
-		if (*str_format == '%')
-			str = format(str_format + 1, &char_written);
-		else
-			str = strdup_till_format(str_format, &char_written);
-		if (str == NULL)
-			return (error(str_list));
-		else
-			ft_lstadd_front(&str_list, ft_lstnew(str));
-		str_format += char_written - previous_char_written;
-	}
+	va_start(args, str_format);
+	str_list = get_str_list(str_format, &args, &char_written);
+	va_end(args);
 	write(0, get_final_str(str_list), char_written);
 	ft_lstclear(&str_list, &free);
 	return (char_written);
